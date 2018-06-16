b082e00056a2611910cd55545f5c60574830fe76
vkill <vkill.net@gmail.com>
2018-06-16 13:02:44 +0800

    Update

--- Sources/AwsSigv4/AwsSigv4Signer.swift
+++ Sources/AwsSigv4/AwsSigv4Signer.swift
@@ -1,18 +1,18 @@
 import struct Foundation.CharacterSet
 
 public final class AwsSigv4Signer {
-    public let service: String
-    public let region: String
-    public let credentials: AwsSigv4Credentials
-    public let unsignedHeaderKeys: Set<String>
+    private let service: String
+    private let region: String
+    private let credentials: AwsSigv4Credentials
+    private let unsignedHeaderKeys: Set<String>
 
-    public let uriEscapePath = true
-    public let applyChecksumHeader = true
+    private let uriEscapePath = true
+    private let applyChecksumHeader = true
 
     public static var queryAllowed = CharacterSet(charactersIn: "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-._~=&")
     public static var pathAllowed = CharacterSet(charactersIn: "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-._~/")
 
-    init(_ service: String, _ region: String, _ credentials: AwsSigv4Credentials, unsignedHeaderKeys: [String] = []) {
+    public init(_ service: String, _ region: String, _ credentials: AwsSigv4Credentials, unsignedHeaderKeys: [String] = []) {
         self.service = service
         self.region = region
         self.credentials = credentials
